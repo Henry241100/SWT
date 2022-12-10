d658fdd795a302bb2ebf10589af4fe2bb045e444
Henry241100 <113590750+Henry241100@users.noreply.github.com>
2022-12-10 16:00:47 +0100

    Add files via upload

---
+++ constants.js
@@ -0,0 +1,19 @@
+const FRAME_RATE = 10;
+const GRID_SIZE = 50;
+
+
+module.exports = {
+  FRAME_RATE,
+  GRID_SIZE,
+  makeid
+}
+
+function makeid(length) {
+   var result           = '';
+   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+   var charactersLength = characters.length;
+   for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+   }
+   return result;
+}
