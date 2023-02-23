ace8260fe8d388b817424c24226ad2cc7dba4b00
Henry241100 <113590750+Henry241100@users.noreply.github.com>
2023-02-23 15:05:50 +0100

    Add files via upload

--- frontend/index.js
+++ frontend/index.js
@@ -91,10 +91,10 @@ function init() {
   ctx = canvas.getContext('2d');
 
 if (window.innerHeight < window.innerWidth){
-    canvas.width = canvas.height = window.innerHeight-100;
+    canvas.width = canvas.height = window.innerHeight;
 }    
 else {
-    canvas.width = canvas.height = window.innerWidth-100;
+    canvas.width = canvas.height = window.innerWidth;
 }
 
   ctx.fillStyle = BG_COLOUR;
